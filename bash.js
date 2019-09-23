58e5f7ad6315914cad056a495ed11c8b95351c3a
Joshua Lemay <jspades93@gmail.com>
2019-09-23 13:14:47 -0400

    A lovely commit message

---
+++ bash.js
@@ -0,0 +1,17 @@
+// process.stdout.write('prompt > ');
+
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim();
+
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt > ');
+// });
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const pwd = process.cwd();
+
+  process.stdout.write(pwd);
+  process.stdout.write('\nprompt > ');
+});
